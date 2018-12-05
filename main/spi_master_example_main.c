69be29bb01572de82643076c43ea9d5d29807a5a
Tim Gremalm <tim@gremalm.se>
2018-12-05 22:29:54 +0100

    Queue SPI packet

--- main/spi_master_example_main.c
+++ main/spi_master_example_main.c
@@ -11,29 +11,49 @@
 #include "soc/gpio_struct.h"
 #include "driver/gpio.h"
 
-#define PIN_NUM_MISO 12
-#define PIN_NUM_MOSI 13
-#define PIN_NUM_CLK  14
-#define PIN_NUM_CS   15
+//#define PIN_NUM_MISO 12
+//#define PIN_NUM_MOSI 13
+//#define PIN_NUM_CLK  14
+//#define PIN_NUM_CS   15
+#define PIN_NUM_MISO 25
+#define PIN_NUM_MOSI 23
+#define PIN_NUM_CLK  19
+#define PIN_NUM_CS   22
 
 static const char *TAG = "spimaster";
 
-static void send_stuff(spi_device_handle_t spi) {
+void send_stuff(spi_device_handle_t spi) {
+	uint8_t buf[4];
+	buf[0] = 1;
+	buf[1] = 2;
+	buf[2] = 3;
+	buf[3] = 4;
+
 	spi_transaction_t trans;
 
 	memset(&trans, 0, sizeof(spi_transaction_t));
-	trans.length=4;
+	trans.length=8*4;
+	trans.tx_buffer=&buf;
 	trans.flags=SPI_TRANS_USE_TXDATA;
-	trans.tx_data[0]=1;
-	trans.tx_data[1]=2;
-	trans.tx_data[2]=3;
-	trans.tx_data[3]=4;
+	trans.user=(void*)0;
 
 	ESP_ERROR_CHECK(spi_device_queue_trans(spi, &trans, portMAX_DELAY));
 
 	spi_transaction_t *recPayload;
 	recPayload = &trans;
 	ESP_ERROR_CHECK(spi_device_get_trans_result(spi, &recPayload, portMAX_DELAY));
+
+	//uint8_t cmd[3];
+	//cmd[0] = 1;
+	//cmd[1] = 2;
+	//cmd[2] = 3;
+
+	//spi_transaction_t t;
+	//memset(&t, 0, sizeof(t));
+	//t.length=24;
+	//t.tx_buffer=&cmd;
+	//t.user=(void*)0;
+	//ESP_ERROR_CHECK(spi_device_polling_transmit(spi, &t));
 }
 
 static void light_control(void *arg) {
@@ -45,13 +65,13 @@ static void light_control(void *arg) {
 		.sclk_io_num=PIN_NUM_CLK,
 		.quadwp_io_num=-1,
 		.quadhd_io_num=-1,
-		.max_transfer_sz=4094
+		.max_transfer_sz=32
 	};
 	spi_device_interface_config_t devcfg = {
-		.clock_speed_hz=1000*1000,
+		.clock_speed_hz=10*1000*1000,
 		.mode=0,
 		.spics_io_num=PIN_NUM_CS,
-		.queue_size=2,							//We want to be able to queue 7 transactions at a time
+		.queue_size=7,
 	};
 	ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
 	ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, &spi));
@@ -59,7 +79,7 @@ static void light_control(void *arg) {
 	while (1) {
 		ESP_LOGI(TAG, "[APP] Send packet");
 		send_stuff(spi);
-		vTaskDelay(100 / portTICK_PERIOD_MS);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
 	}
 }
 
