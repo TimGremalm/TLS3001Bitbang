32afeb1b6d4ac3464d9d6d8e3ca28e247feac7fb
Tim Gremalm <tim@gremalm.se>
2018-12-10 04:01:28 +0100

    RMT send TLS3001 reset, delay and sync

--- main/spi_master_example_main.c
+++ main/spi_master_example_main.c
@@ -11,7 +11,6 @@
 
 #define RMT_TX_CHANNEL RMT_CHANNEL_0
 #define RMT_TX_GPIO 18
-#define SAMPLE_CNT  (10)
 
 static const char *TAG = "spimaster";
 
@@ -29,7 +28,6 @@ inline rgbVal makeRGBVal(uint8_t r, uint8_t g, uint8_t b) {
 	v.b = b;
 	return v;
 }
-
 
 rmt_item32_t items[] = {
 	// E : dot
@@ -49,10 +47,45 @@ rmt_item32_t items[] = {
 	{{{ 32767, 1, 32767, 1 }}},
 	{{{ 32767, 1, 32767, 0 }}}, // dash
 	{{{ 32767, 1, 32767, 0 }}}, // dot
-
 	// RMT end marker
 	{{{ 0, 1, 0, 0 }}}
 };
+
+#define NUM(a) (sizeof(a) / sizeof(*a))
+#define RMT_BLOCK_LEN	32
+#define data_one  {{{ 255, 1, 255, 0 }}}
+#define data_zero {{{ 255, 0, 255, 1 }}}
+#define data_5usblank {{{ 20000, 0, 18000, 0 }}}
+
+rmt_item32_t packet_resetdevice[] = {
+	data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one,
+	data_zero,
+	data_one,
+	data_zero,
+	data_zero
+}; //Reset device (19 bits, 15 x 0b1, 1 x 0b0, 1 x 0b1 & 2 x 0b0)
+
+rmt_item32_t packet_blank[] = {
+	data_5usblank
+}; //Blank
+
+rmt_item32_t packet_delayresetsync[] = {
+	data_5usblank, data_5usblank
+}; //Delay (1mS) between Reset device and Synchronize device
+
+rmt_item32_t packet_syncdevice[] = {
+	data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one,
+	data_zero, data_zero, data_zero,
+	data_one,
+	data_zero, data_zero, data_zero, data_zero, data_zero, data_zero, data_zero, data_zero, data_zero, data_zero, data_zero
+}; //Synchronize device (30 bits, 15 x 0b1, 3 x 0b0, 1 x 0b1 & 11 x 0b0)
+
+rmt_item32_t packet_startdata[] = {
+	data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one, data_one,
+	data_zero, data_zero,
+	data_one,
+	data_zero
+}; //Start of data (19 bits, 15 x 0b1, 2 x 0b0, 1 x 0b1 & 1 x 0b0)
 
 //Convert uint8_t type of data to rmt format data.
 static void IRAM_ATTR u8_to_rmt(const void* src, rmt_item32_t* dest, size_t src_size, size_t wanted_num, size_t* translated_size, size_t* item_num) {
@@ -93,35 +126,31 @@ static void light_control(void *arg) {
 	config.gpio_num = RMT_TX_GPIO;
 	config.mem_block_num = 1;
 	config.tx_config.loop_en = 0;
-	// enable the carrier to be able to hear the Morse sound
-	// if the RMT_TX_GPIO is connected to a speaker
-	config.tx_config.carrier_en = 1;
+	config.tx_config.carrier_en = 0;
 	config.tx_config.idle_output_en = 1;
 	config.tx_config.idle_level = 0;
-	config.tx_config.carrier_duty_percent = 50;
-	// set audible career frequency of 611 Hz
-	// actually 611 Hz is the minimum, that can be set
-	// with current implementation of the RMT API
-	config.tx_config.carrier_freq_hz = 611;
-	config.tx_config.carrier_level = 1;
-	// set the maximum clock divider to be able to output
-	// RMT pulses in range of about one hundred milliseconds
-	config.clk_div = 255;
+	config.clk_div = 1;
 
 	ESP_ERROR_CHECK(rmt_config(&config));
 	ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
-	ESP_ERROR_CHECK(rmt_translator_init(config.channel, u8_to_rmt));
+	//ESP_ERROR_CHECK(rmt_translator_init(config.channel, u8_to_rmt));
 
-	int number_of_items = sizeof(items) / sizeof(items[0]);
-	const uint8_t sample[SAMPLE_CNT] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	//int number_of_items = sizeof(items) / sizeof(items[0]);
+	//int number_of_items = sizeof(packet_startdata) / sizeof(packet_startdata[0]);
+
+	//Blank RMT buffer
+	//for (int i = 0; i<64; i++) {
+	//	ESP_ERROR_CHECK(rmt_fill_tx_items(RMT_TX_CHANNEL, packet_blank, 1, i*RMT_BLOCK_LEN));
+	//}
 
 	ESP_LOGI(TAG, "[APP] Init done");
 	while (1) {
 		ESP_LOGI(TAG, "[APP] Send packet");
-		ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, items, number_of_items, true));
+		//ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, items, number_of_items, true));
+		ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, packet_resetdevice, NUM(packet_resetdevice), true));
+		ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, packet_delayresetsync, NUM(packet_delayresetsync), true));
+		ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, packet_syncdevice, NUM(packet_syncdevice), true));
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		ESP_ERROR_CHECK(rmt_write_sample(RMT_TX_CHANNEL, sample, SAMPLE_CNT, true));
-		vTaskDelay(2000 / portTICK_PERIOD_MS);
 	}
 }
 
